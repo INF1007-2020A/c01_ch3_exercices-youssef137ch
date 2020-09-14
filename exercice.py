ac24da3342ae38cac7f704ef2c502c6ebce41891
youssef137ch <youssef.chourifi@polymtl.ca>
2020-09-14 19:50:30 +0000

    erreur de donnée en print au #2

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return math.radians(angle_degs + angle_mins / 60 + angle_secs / 3600)
+    return math.radians(angle_degs + angle_mins /60 + angle_secs / 3600)
 
 
 def to_degrees(angle_rads: float) -> tuple:
@@ -30,7 +30,7 @@ def to_farenheit(temperature: float) -> float:
 def main() -> None:
     print(f"Moyenne des nombres 2, 4, 6: {average(2.1, 4.3, 6.5)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
+    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(100, 2, 45)}")
     
     degrees, minutes, seconds = to_degrees(1.0)
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
