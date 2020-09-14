5ab1215cdf8921480166dcf9241652f74e7cbe81
youssef137ch <youssef.chourifi@polymtl.ca>
2020-09-14 09:36:42 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return math.radians(angle_degs) + math.radians(angle_mins/60), math.radians(angle_secs/3600)
+    return math.radians(angle_degs) + math.radians(angle_mins/60) + math.radians(angle_secs/3600)
 
 
 def to_degrees(angle_rads: float) -> tuple:
