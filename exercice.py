950c3cdb2953ace59b3e1894f6b09bafafb07624
youssef137ch <youssef.chourifi@polymtl.ca>
2020-09-14 09:08:56 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    return temperature * 1.8 + 32
+    return temperature * 1.8 + 
 
 
 def main() -> None:
