fb929c8364b01dd71ddc212d552cf624cbebc43a
youssef137ch <youssef.chourifi@polymtl.ca>
2020-09-14 08:55:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,7 @@ import math
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return 
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
